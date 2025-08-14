5453571a90edb38e2d520688ab23f638b232958f
miurann <mn.camissha@gmail.com>
2025-08-14 16:33:07 +0900

    fix:seedの内容修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,75 +1,75 @@
 CoverImage.create!(
   [
     {
-      image: File.open('./app/db/files/book28.png'),
+      image: File.open('./db/files/book28.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book29.png'),
+      image: File.open('./db/files/book29.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book30.png'),
+      image: File.open('./db/files/book30.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book31.png'),
+      image: File.open('./db/files/book31.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book32.png'),
+      image: File.open('./db/files/book32.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book33.png'),
+      image: File.open('./db/files/book33.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book34.png'),
+      image: File.open('./db/files/book34.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book35.png'),
+      image: File.open('./db/files/book35.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book36.png'),
+      image: File.open('./db/files/book36.png'),
       standard: true
     },
     {
-      image: File.open('./app/db/files/book19front.png'),
+      image: File.open('./db/files/book19front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book20front.png'),
+      image: File.open('./db/files/book20front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book21front.png'),
+      image: File.open('./db/files/book21front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book22front.png'),
+      image: File.open('./db/files/book22front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book23front.png'),
+      image: File.open('./db/files/book23front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book24front.png'),
+      image: File.open('./db/files/book24front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book25front.png'),
+      image: File.open('./db/files/book25front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book26front.png'),
+      image: File.open('./db/files/book26front.png'),
       standard: false
     },
     {
-      image: File.open('./app/db/files/book27front.png'),
+      image: File.open('./db/files/book27front.png'),
       standard: false
     },
   ]
