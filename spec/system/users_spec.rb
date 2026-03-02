37d53a519227d5e5cd77ced3145e6f0b7f41b36a
miurann <mn.camissha@gmail.com>
2026-03-02 16:28:21 +0900

    fix:ヘッダーの不必要な行を削除

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+=begin
+  scenario "ユーザーは新しいユーザーアカウントを作成する" do
+    visit root_path
+    click_link
+=end
+end
